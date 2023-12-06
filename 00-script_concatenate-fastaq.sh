afb7c2b760ab7163e65c7ab86c4f39d048cdfde7
arnaud04fr <53262750+arnaud04fr@users.noreply.github.com>
2023-12-06 17:36:59 +0100

    Update 00-script_concatenate-fastaq.sh

--- 00-script_concatenate-fastaq.sh
+++ 00-script_concatenate-fastaq.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=arnaud04@gmail.com
+#SBATCH --mail-user=youremail@mail.com
 
 # le script va s'arrêter
 # - à la première erreur
@@ -13,10 +13,10 @@ set -euo pipefail
 
 
 # Specify the input FASTQ.gz files
-input_files=("SRR18918146.fastq.gz" "SRR18918147.fastq.gz" "SRR18918148.fastq.gz" "SRR18918149.fastq.gz")
+input_files=("XX-1.fastq.gz" "XX-2.fastq.gz" "XX-3.fastq.gz")
 
 # Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918146.fastq.gz"
+output_file="cXX.fastq.gz"
 
 echo "########################################"
 echo "###    Concatenation $input_files   ###"   
@@ -34,144 +34,3 @@ rm "temp.fastq"
 echo "Concatenation and compression complete. Output saved to $output_file."
 
 ###############################################################################################
-
-# Specify the input FASTQ.gz files
-input_files=("SRR18918150.fastq.gz" "SRR18918151.fastq.gz" "SRR18918152.fastq.gz" "SRR18918153.fastq.gz")
-
-echo "########################################"
-echo "###    Concatenation $input_files   ###"   
-echo "########################################"
-
-# Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918150.fastq.gz"
-
-# Concatenate and compress the input files
-for file in "${input_files[@]}"; do
-  zcat "$file" >> "temp.fastq"
-done
-
-# Compress the concatenated file and remove the temporary file
-gzip -c "temp.fastq" > "$output_file"
-rm "temp.fastq"
-
-echo "Concatenation and compression complete. Output saved to $output_file."
-
-###############################################################################################
-
-# Specify the input FASTQ.gz files
-input_files=("SRR18918154.fastq.gz" "SRR18918155.fastq.gz" "SRR18918156.fastq.gz" "SRR18918157.fastq.gz")
-
-echo "########################################"
-echo "###    Concatenation $input_files   ###"   
-echo "########################################"
-
-# Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918154.fastq.gz"
-
-# Concatenate and compress the input files
-for file in "${input_files[@]}"; do
-  zcat "$file" >> "temp.fastq"
-done
-
-# Compress the concatenated file and remove the temporary file
-gzip -c "temp.fastq" > "$output_file"
-rm "temp.fastq"
-
-echo "Concatenation and compression complete. Output saved to $output_file."
-
-###############################################################################################
-
-# Specify the input FASTQ.gz files
-input_files=("SRR18918158.fastq.gz" "SRR18918159.fastq.gz" "SRR18918160.fastq.gz" "SRR18918161.fastq.gz")
-
-
-echo "########################################"
-echo "###    Concatenation $input_files   ###"   
-echo "########################################"
-
-# Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918158.fastq.gz"
-
-# Concatenate and compress the input files
-for file in "${input_files[@]}"; do
-  zcat "$file" >> "temp.fastq"
-done
-
-# Compress the concatenated file and remove the temporary file
-gzip -c "temp.fastq" > "$output_file"
-rm "temp.fastq"
-
-echo "Concatenation and compression complete. Output saved to $output_file."
-
-###############################################################################################
-
-# Specify the input FASTQ.gz files
-input_files=("SRR18918162.fastq.gz" "SRR18918163.fastq.gz" "SRR18918164.fastq.gz" "SRR18918165.fastq.gz")
-
-
-echo "########################################"
-echo "###    Concatenation $input_files   ###"   
-echo "########################################"
-
-# Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918162.fastq.gz"
-
-# Concatenate and compress the input files
-for file in "${input_files[@]}"; do
-  zcat "$file" >> "temp.fastq"
-done
-
-# Compress the concatenated file and remove the temporary file
-gzip -c "temp.fastq" > "$output_file"
-rm "temp.fastq"
-
-echo "Concatenation and compression complete. Output saved to $output_file."
-
-###############################################################################################
-
-# Specify the input FASTQ.gz files
-input_files=("SRR18918166.fastq.gz" "SRR18918167.fastq.gz" "SRR18918168.fastq.gz" "SRR18918169.fastq.gz")
-
-
-echo "########################################"
-echo "###    Concatenation $input_files   ###"   
-echo "########################################"
-
-# Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918166.fastq.gz"
-
-# Concatenate and compress the input files
-for file in "${input_files[@]}"; do
-  zcat "$file" >> "temp.fastq"
-done
-
-# Compress the concatenated file and remove the temporary file
-gzip -c "temp.fastq" > "$output_file"
-rm "temp.fastq"
-
-echo "Concatenation and compression complete. Output saved to $output_file."
-
-
-###############################################################################################
-
-# Specify the input FASTQ.gz files
-input_files=("SRR18918170.fastq.gz" "SRR18918171.fastq.gz" "SRR18918172.fastq.gz" "SRR18918173.fastq.gz")
-
-
-echo "########################################"
-echo "###    Concatenation $input_files   ###"   
-echo "########################################"
-
-# Specify the output file (compressed FASTQ.gz)
-output_file="cSRR18918170.fastq.gz"
-
-# Concatenate and compress the input files
-for file in "${input_files[@]}"; do
-  zcat "$file" >> "temp.fastq"
-done
-
-# Compress the concatenated file and remove the temporary file
-gzip -c "temp.fastq" > "$output_file"
-rm "temp.fastq"
-
-echo "Concatenation and compression complete. Output saved to $output_file."
